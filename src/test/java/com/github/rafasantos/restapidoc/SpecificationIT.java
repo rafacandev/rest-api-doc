23d115ffc1969876d0b34fbacee1d914af1ae45f
Rafael Santos <rafael.santos.bra@gmail.com>
2018-10-06 13:24:55 -0400

    Updated .gitignore

--- src/test/java/com/github/rafasantos/restapidoc/SpecificationIT.java
+++ src/test/java/com/github/rafasantos/restapidoc/SpecificationIT.java
@@ -1,10 +1,10 @@
 package com.github.rafasantos.restapidoc;
 
 import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 
 import io.restassured.RestAssured;
+import static org.mockito.Mockito.*;
 
 public class SpecificationParserTest {
 	
@@ -38,6 +38,23 @@ public class SpecificationParserTest {
 		SpecificationParser fixture = new SpecificationParser(filter);
 		String actualResult = fixture.requestAsText();
 		assertEquals("GET http://www.mocky.io/v2/5a08a8cb3200000a0a138011\n", actualResult);
+	}
+
+	@Test
+	public void shouldGetBasicRequestHtmlSnippet() {
+		String textRequest = "Text Request";
+		String curlRequest = "curl Request";
+		String textResponse = "Text Response";
+
+		SpecificationFilter filter = mock(SpecificationFilter.class);
+		SpecificationParser fixture = spy(new SpecificationParser(filter));
+		doReturn(textRequest).when(fixture).requestAsText();
+		doReturn(curlRequest).when(fixture).requestAsCurl();
+		doReturn(textResponse).when(fixture).responseAsText();
+
+		String actualResult = fixture.requestHtmlSnippet();
+		String expectedResult = TestUtil.buildHtmlSnippet(textRequest, textRequest, curlRequest, textResponse);
+		assertEquals(expectedResult, actualResult);
 	}
 
 }
